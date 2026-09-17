64f4921c46905ca3e6a8d462a699289e5fa83441
guest <143449854+nicho-roy@users.noreply.github.com>
2026-09-17 17:22:13 -0400

    minor changes

--- topics/instruction-challenge/js/script.js
+++ topics/instruction-challenge/js/script.js
@@ -9,10 +9,13 @@
 "use strict";
 const amplitude = 200;
 const angleStep = 0.01; //SPEED, 0.01 idealy
+const starCount = 100;
+const starDistance = 100;
 
 let angle = 0;
 let earthImage
 let moonImage;
+let skyboxImage;
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
@@ -20,9 +23,10 @@ let moonImage;
 async function setup() {
     earthImage = await loadImage('./assets/images/earthImage.jpg');
     moonImage = await loadImage('./assets/images/moonImage.jpg');
+    skyboxImage = await loadImage('./assets/images/endPortal.jpg');
 
     createCanvas(720,400,WEBGL);
-    noStroke(); //removes mesh outline
+    noStroke(); //remov. es mesh outline
     describe('Earth and moon celestial model');
 }
 
@@ -31,15 +35,40 @@ async function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(200);
+    background(0);
     orbitControl();
+
+    //# SKYBOX
+    
+    randomSeed(1);
+    let randX, randY, randZ;
+    for(let i = 0; i < 1; i++) {
+        //TODON: add random colors for stars
+        push();
+        randX = random(PI*2);
+        randY = random(PI*2);
+        randZ = random(PI*2);
+        
+        fill(255,240,200);
+        // translate(randX*amplitude,randY*amplitude,randZ*amplitude);
+        
+        rotateX(randX);
+        rotateY(randY);
+        rotateZ(randZ);
+        
+        sphere(10);
+        pop();
+    }
+
+
+    
     
     //# EARTH
     push();
     //rotate earth
     rotateX(0.1);
     rotateZ(0.1);
-    rotateY(angle);
+    rotateY(angle/3);
 
     //texture
     texture(earthImage);
