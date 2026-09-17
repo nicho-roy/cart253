e63d3fb006a4284333e9f09530cf7f1e0dca8dac
guest <143449854+nicho-roy@users.noreply.github.com>
2026-09-17 15:53:47 -0400

    Instructions: made a simple earth and moon orbit model

---
+++ topics/instruction-challenge/js/script.js
@@ -0,0 +1,45 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+const amplitude = 200;
+const angleStep = 0.01; //SPEED, 0.01 idealy
+
+let angle = 0;
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(720,400,WEBGL)
+    //noStroke(); //removes mesh outline
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(200);
+    orbitControl();
+    
+    //# EARTH
+
+    //draw earth
+    sphere();
+
+    //# MOON
+    //moves the sphere
+    angle +=  angleStep;
+    let xPos = cos(angle);
+    let yPos = sin(angle);
+    translate(amplitude*xPos, amplitude/10*yPos, amplitude*yPos); 
+
+    //draw moon
+    sphere(10);
+}
