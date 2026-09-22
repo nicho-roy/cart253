6c5bf521471e8fa1a204e07a3ea7e82b212b8286
nichoroy <nicholas@patroy.ca>
2026-09-22 15:37:41 -0400

    finished prototype 2 and renanamed it

--- prototypes/instructions/sky_full_of_stars/js/script.js
+++ prototypes/instructions/sky_full_of_stars/js/script.js
@@ -9,8 +9,8 @@
 "use strict";
 const amplitude = 200;
 const angleStep = 0.01; //SPEED, 0.01 idealy
-const starCount = 100;
-const starDistance = 100;
+const starCount = 4000;
+const starDistance = 5000;
 
 let angle = 0;
 let earthImage
@@ -42,12 +42,62 @@ function draw() {
 
     //# SKYBOX
 
-    //skybox
-    push();
-    texture(earthImage);
-    sphere(3000);
-    pop();
+    // //skybox
+    // push();
+    // texture(earthImage);
+    // sphere(3000);
+    // pop();
     
+    //# EARTH
+    push();
+    //rotate earth
+    rotateX(0.1);
+    rotateZ(0.1);
+    rotateY(angle/3);
+
+    //texture
+    texture(earthImage);
+
+    //draw earth
+    sphere();
+
+    pop();
+
+    //# MOONS
+    push();
+    //moves the sphere
+    angle +=  angleStep;
+    let xPos = cos(angle);
+    let yPos = sin(angle);
+    translate(amplitude*xPos, amplitude/10*yPos, amplitude*yPos); 
+
+    //texture
+    texture(moonImage);
+
+    //draw moon
+    sphere(10);
+
+    pop();
+
+
+    push();
+
+    randomSeed(1);
+    for(let i = 0; i < starCount; i++) {
+        push();
+        let theta = random(2*PI);
+        let phi = acos(random(-1, 1)); // gives uniform spherical distribution
+        let xPos = sin(phi) * cos(theta);
+        let yPos = sin(phi) * sin(theta);
+        let zPos = cos(phi);
+        translate(starDistance*xPos, starDistance*yPos, starDistance*zPos); 
+        texture(sunImage);
+        sphere(random(3,5));
+        pop();
+    }
+
+    pop();
+
     // randomSeed(1);
     // let randX, randY, randZ;
     // for(let i = 0; i < 1; i++) {
@@ -67,38 +117,6 @@ function draw() {
     //     sphere(10);
     //     pop();
     // }
-
-    
-    //# EARTH
-    push();
-    //rotate earth
-    rotateX(0.1);
-    rotateZ(0.1);
-    rotateY(angle/3);
-
-    //texture
-    texture(sunImage);
-
-    //draw earth
-    sphere();
-
-    pop();
-
-    //# MOON
-    push();
-    //moves the sphere
-    angle +=  angleStep;
-    let xPos = cos(angle);
-    let yPos = sin(angle);
-    translate(amplitude*xPos, amplitude/10*yPos, amplitude*yPos); 
-
-    //texture
-    texture(moonImage);
-
-    //draw moon
-    sphere(10);
-
-    pop();
 
     console.log('frame');
 }
